1fc9fa602ad58f735b67fb59cb4a36ec8409e4e2
ADDFa <fratamaadha18@gmail.com>
2024-07-30 16:50:30 +0700

    fitur hapus controller

--- routes/api.php
+++ routes/api.php
@@ -100,4 +100,4 @@ Route::middleware("jwt.verify")->group(function () {
     });
 });
 
-Route::delete("report/delete-all", [ReportController::class, "destroy"]);
+Route::delete("report/remove/all", [ReportController::class, "destroy"]);
