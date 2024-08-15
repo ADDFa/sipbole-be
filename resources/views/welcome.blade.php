fda3fe024a174cb3f3d95a84429f34b5b7334712
ADDFa <fratamaadha18@gmail.com>
2024-08-15 14:21:03 +0700

    memasukkan frontend

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     <link rel="icon" type="image/png" href="/logo.png" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>SI_PAL</title>
-    <script type="module" crossorigin src="/assets/index-BtUo9LwR.js"></script>
+    <script type="module" crossorigin src="/assets/index-IzpyNcnp.js"></script>
     <link rel="stylesheet" crossorigin href="/assets/index-BDlq7uLt.css">
 </head>
 
