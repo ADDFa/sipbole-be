cfab835bb5e0a894dcf1716aa613d3779e231262
ADDFa <fratamaadha18@gmail.com>
2024-08-18 21:46:14 +0700

    mengubah nav di fe

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     <link rel="icon" type="image/png" href="/logo.png" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>SI_PAL</title>
-    <script type="module" crossorigin src="/assets/index-Fwde6Bcf.js"></script>
+    <script type="module" crossorigin src="/assets/index-0X7WxoUB.js"></script>
     <link rel="stylesheet" crossorigin href="/assets/index-C9-FSur9.css">
 </head>
 
