d512ef069fd76e4ced9199965aa32d6a71a403da
ADDFa <fratamaadha18@gmail.com>
2024-08-19 15:26:43 +0700

    menambahkan fe ke views

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,12 +6,11 @@
     <link rel="icon" type="image/png" href="/logo.png" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>SI_PAL</title>
-    <script type="module" crossorigin src="/assets/index-CS1zJzP3.js"></script>
-    <link rel="stylesheet" crossorigin href="/assets/index-C9-FSur9.css">
 </head>
 
 <body>
     <div id="root"></div>
+    <script type="module" src="/src/main.tsx"></script>
 </body>
 
 </html>
